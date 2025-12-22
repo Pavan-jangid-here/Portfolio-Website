79e9141fde798267cffc74f35c183e1b95e2510e
pavan-jangid-here <pavan5.jan@gmail.com>
2025-12-22 06:09:05 +0530

    server bug

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the homepage
+// Homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Catch-all route for SPA
+// SPA catch-all
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export for Vercel serverless
+module.exports = app;
